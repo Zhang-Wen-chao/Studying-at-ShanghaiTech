a7571a55fdb1b144b0e2491da60e66628298a616
Zhang-Wen-chao <zhangwch2022@shanghaitech.edu.cn>
2023-12-12 07:38:54 +0000

    update

---
+++ AI for Science and Engineering_Zheng Jie/homework/hw2/gnn.py
@@ -0,0 +1,141 @@
+import torch
+import torch.nn as nn
+import dgl
+from dgllife.utils import smiles_to_bigraph
+from rdkit import Chem
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+from torch.optim.lr_scheduler import StepLR
+from dgl.nn import GATConv
+
+# 确保DG-LifeSci库已安装
+# pip install dgllife dgl rdkit
+
+# 将SMILES字符串转换为图的函数
+def smiles_to_graph(smiles_string):
+    if isinstance(smiles_string, str):
+        mol = Chem.MolFromSmiles(smiles_string)
+        if mol is None:
+            return None
+        # 直接从SMILES字符串生成图
+        return smiles_to_bigraph(smiles_string)
+    else:
+        return None
+
+def collate(samples):
+    graphs, labels = map(list, zip(*samples))
+    batched_graph = dgl.batch(graphs)
+    batched_labels = torch.tensor(labels)
+    return batched_graph, batched_labels
+
+# 创建数据集类
+class QSARDataset(Dataset):
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+        self.dataframe['graph'] = self.dataframe['Drug'].apply(smiles_to_graph)
+        self.dataframe = self.dataframe[self.dataframe['graph'].notnull()]
+        self.graphs = self.dataframe['graph'].tolist()
+        self.labels = torch.tensor(self.dataframe['Label'].values, dtype=torch.long)  # 将标签转换为长整型
+    def __len__(self):
+        return len(self.graphs)
+
+    def __getitem__(self, idx):
+        return self.graphs[idx], self.labels[idx]
+
+class QSARGNN(nn.Module):
+    def __init__(self, in_feats, hidden_size, num_classes, num_heads=4):
+        super(QSARGNN, self).__init__()
+        # 使用GATConv作为图注意力层
+        self.gat1 = GATConv(in_feats, hidden_size, num_heads=num_heads)
+        self.gat2 = GATConv(hidden_size * num_heads, hidden_size, num_heads=num_heads)
+        # Additional GATConv layers
+        self.gat3 = GATConv(hidden_size * num_heads, hidden_size, num_heads=num_heads)
+        self.gat4 = GATConv(hidden_size * num_heads, hidden_size, num_heads=num_heads)
+        self.classify = nn.Linear(hidden_size * num_heads, num_classes)
+
+    def forward(self, g):
+        h = g.in_degrees().view(-1, 1).float()
+        # 使用GAT层
+        h = self.gat1(g, h).flatten(1)
+        h = torch.relu(h)
+        h = self.gat2(g, h).flatten(1)
+        h = torch.relu(h)
+        h = self.gat3(g, h).flatten(1)  # Additional layer
+        h = torch.relu(h)
+        h = self.gat4(g, h).flatten(1)  # Additional layer
+        h = torch.relu(h)
+        g.ndata['h'] = h
+        hg = dgl.mean_nodes(g, 'h')
+        return self.classify(hg)
+
+# 加载数据
+train_data = pd.read_csv('QSAR_train.csv')
+test_data = pd.read_csv('QSAR_test.csv')
+# train_data = pd.read_csv('mini_train.csv')
+# test_data = pd.read_csv('mini_test.csv')
+# 创建数据集和数据加载器
+train_dataset = QSARDataset(train_data)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=collate)
+
+
+# 模型、损失函数和优化器
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = QSARGNN(in_feats=1, hidden_size=64, num_classes=2).to(device)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+scheduler = StepLR(optimizer, step_size=5, gamma=0.5)
+# 训练模型
+num_epochs = 10
+for epoch in range(num_epochs):
+    model.train()
+    for batched_graph, labels in train_loader:
+        batched_graph = batched_graph.to(device)
+        labels = labels.to(device)
+        logits = model(batched_graph)
+        loss = loss_fn(logits, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    scheduler.step()
+    print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}")
+
+# 模型训练后进行训练集评估
+model.eval()
+train_pred_probs = []
+train_labels_list = []
+with torch.no_grad():
+    for batched_graph, labels in train_loader:
+        batched_graph = batched_graph.to(device)
+        labels = labels.to(device)
+
+        logits = model(batched_graph)
+        probs = torch.nn.functional.softmax(logits, dim=1)[:, 1]  # 获取正类的概率
+        train_pred_probs.append(probs.cpu())
+        train_labels_list.append(labels.cpu())
+
+# 将预测概率列表和标签列表转换为Tensor
+train_pred_probs = torch.cat(train_pred_probs)
+train_labels = torch.cat(train_labels_list)
+
+# 计算ROC AUC值
+from sklearn.metrics import roc_auc_score
+train_roc_auc = roc_auc_score(train_labels.numpy(), train_pred_probs.numpy())
+print(f"Training ROC AUC: {train_roc_auc}")
+
+# 测试集预测
+test_data['graph'] = test_data['Drug'].apply(smiles_to_graph)
+test_graphs = test_data['graph'].tolist()
+test_dataset = dgl.batch(test_graphs).to(device)
+
+model.eval()
+with torch.no_grad():
+    logits = model(test_dataset)
+    predicted_probs = torch.nn.functional.softmax(logits, dim=1)[:, 1].cpu().numpy()
+
+# 保存预测结果
+predictions = pd.DataFrame({
+    'Drug_ID': test_data['Drug_ID'],
+    'Predicted_Score': predicted_probs
+})
+predictions.to_csv('predicted_results.csv', index=False)
---
+++ AI for Science and Engineering_Zheng Jie/homework/hw2/keras_mpnn.py
@@ -0,0 +1,466 @@
+import os
+
+# Temporary suppress tf logs
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings
+from rdkit import Chem
+from rdkit import RDLogger
+from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem.Draw import MolsToGridImage
+
+# Temporary suppress warnings and RDKit logs
+warnings.filterwarnings("ignore")
+RDLogger.DisableLog("rdApp.*")
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+# csv_path = keras.utils.get_file(
+#     "BBBP.csv", "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/BBBP.csv"
+# )
+# csv_path = "mini_train_bbbp.csv"
+csv_path = "QSAR_train_bbbp.csv"
+df = pd.read_csv(csv_path, usecols=[0, 1, 2])
+
+# df = pd.read_csv(csv_path, usecols=[1, 2, 3])
+# print(df.iloc[96:104])
+
+class Featurizer:
+    def __init__(self, allowable_sets):
+        self.dim = 0
+        self.features_mapping = {}
+        for k, s in allowable_sets.items():
+            s = sorted(list(s))
+            self.features_mapping[k] = dict(zip(s, range(self.dim, len(s) + self.dim)))
+            self.dim += len(s)
+
+    def encode(self, inputs):
+        output = np.zeros((self.dim,))
+        for name_feature, feature_mapping in self.features_mapping.items():
+            feature = getattr(self, name_feature)(inputs)
+            if feature not in feature_mapping:
+                continue
+            output[feature_mapping[feature]] = 1.0
+        return output
+
+
+class AtomFeaturizer(Featurizer):
+    def __init__(self, allowable_sets):
+        super().__init__(allowable_sets)
+
+    def symbol(self, atom):
+        return atom.GetSymbol()
+
+    def n_valence(self, atom):
+        return atom.GetTotalValence()
+
+    def n_hydrogens(self, atom):
+        return atom.GetTotalNumHs()
+
+    def hybridization(self, atom):
+        return atom.GetHybridization().name.lower()
+
+
+class BondFeaturizer(Featurizer):
+    def __init__(self, allowable_sets):
+        super().__init__(allowable_sets)
+        self.dim += 1
+
+    def encode(self, bond):
+        output = np.zeros((self.dim,))
+        if bond is None:
+            output[-1] = 1.0
+            return output
+        output = super().encode(bond)
+        return output
+
+    def bond_type(self, bond):
+        return bond.GetBondType().name.lower()
+
+    def conjugated(self, bond):
+        return bond.GetIsConjugated()
+
+
+atom_featurizer = AtomFeaturizer(
+    allowable_sets={
+        "symbol": {"B", "Br", "C", "Ca", "Cl", "F", "H", "I", "N", "Na", "O", "P", "S"},
+        "n_valence": {0, 1, 2, 3, 4, 5, 6},
+        "n_hydrogens": {0, 1, 2, 3, 4},
+        "hybridization": {"s", "sp", "sp2", "sp3"},
+    }
+)
+
+bond_featurizer = BondFeaturizer(
+    allowable_sets={
+        "bond_type": {"single", "double", "triple", "aromatic"},
+        "conjugated": {True, False},
+    }
+)
+def molecule_from_smiles(smiles):
+    # MolFromSmiles(m, sanitize=True) should be equivalent to
+    # MolFromSmiles(m, sanitize=False) -> SanitizeMol(m) -> AssignStereochemistry(m, ...)
+    molecule = Chem.MolFromSmiles(smiles, sanitize=False)
+
+    # If sanitization is unsuccessful, catch the error, and try again without
+    # the sanitization step that caused the error
+    flag = Chem.SanitizeMol(molecule, catchErrors=True)
+    if flag != Chem.SanitizeFlags.SANITIZE_NONE:
+        Chem.SanitizeMol(molecule, sanitizeOps=Chem.SanitizeFlags.SANITIZE_ALL ^ flag)
+
+    Chem.AssignStereochemistry(molecule, cleanIt=True, force=True)
+    return molecule
+
+
+def graph_from_molecule(molecule):
+    # Initialize graph
+    atom_features = []
+    bond_features = []
+    pair_indices = []
+
+    for atom in molecule.GetAtoms():
+        atom_features.append(atom_featurizer.encode(atom))
+
+        # Add self-loops
+        pair_indices.append([atom.GetIdx(), atom.GetIdx()])
+        bond_features.append(bond_featurizer.encode(None))
+
+        for neighbor in atom.GetNeighbors():
+            bond = molecule.GetBondBetweenAtoms(atom.GetIdx(), neighbor.GetIdx())
+            pair_indices.append([atom.GetIdx(), neighbor.GetIdx()])
+            bond_features.append(bond_featurizer.encode(bond))
+
+    return np.array(atom_features), np.array(bond_features), np.array(pair_indices)
+
+
+def graphs_from_smiles(smiles_list):
+    # Initialize graphs
+    atom_features_list = []
+    bond_features_list = []
+    pair_indices_list = []
+
+    for smiles in smiles_list:
+        molecule = molecule_from_smiles(smiles)
+        atom_features, bond_features, pair_indices = graph_from_molecule(molecule)
+
+        atom_features_list.append(atom_features)
+        bond_features_list.append(bond_features)
+        pair_indices_list.append(pair_indices)
+
+    # Convert lists to ragged tensors for tf.data.Dataset later on
+    return (
+        tf.ragged.constant(atom_features_list, dtype=tf.float32),
+        tf.ragged.constant(bond_features_list, dtype=tf.float32),
+        tf.ragged.constant(pair_indices_list, dtype=tf.int64),
+    )
+
+
+# Shuffle array of indices ranging from 0 to 2049
+permuted_indices = np.random.permutation(np.arange(df.shape[0]))
+
+# Train set: 80 % of data
+train_index = permuted_indices[: int(df.shape[0] * 0.8)]
+x_train = graphs_from_smiles(df.iloc[train_index].smiles)
+y_train = df.iloc[train_index].p_np
+
+# Valid set: 19 % of data
+valid_index = permuted_indices[int(df.shape[0] * 0.8) : int(df.shape[0] * 0.99)]
+x_valid = graphs_from_smiles(df.iloc[valid_index].smiles)
+y_valid = df.iloc[valid_index].p_np
+
+# Test set: 1 % of data
+test_index = permuted_indices[int(df.shape[0] * 0.99) :]
+x_test = graphs_from_smiles(df.iloc[test_index].smiles)
+y_test = df.iloc[test_index].p_np
+
+# print(f"Name:\t{df.name[100]}\nSMILES:\t{df.smiles[100]}\nBBBP:\t{df.p_np[100]}")
+# molecule = molecule_from_smiles(df.iloc[100].smiles)
+# print("Molecule:", molecule)
+
+# graph = graph_from_molecule(molecule)
+# print("Graph (including self-loops):")
+# print("\tatom features\t", graph[0].shape)
+# print("\tbond features\t", graph[1].shape)
+# print("\tpair indices\t", graph[2].shape)
+
+def prepare_batch(x_batch, y_batch):
+    """Merges (sub)graphs of batch into a single global (disconnected) graph
+    """
+
+    atom_features, bond_features, pair_indices = x_batch
+
+    # Obtain number of atoms and bonds for each graph (molecule)
+    num_atoms = atom_features.row_lengths()
+    num_bonds = bond_features.row_lengths()
+
+    # Obtain partition indices (molecule_indicator), which will be used to
+    # gather (sub)graphs from global graph in model later on
+    molecule_indices = tf.range(len(num_atoms))
+    molecule_indicator = tf.repeat(molecule_indices, num_atoms)
+
+    # Merge (sub)graphs into a global (disconnected) graph. Adding 'increment' to
+    # 'pair_indices' (and merging ragged tensors) actualizes the global graph
+    gather_indices = tf.repeat(molecule_indices[:-1], num_bonds[1:])
+    increment = tf.cumsum(num_atoms[:-1])
+    increment = tf.pad(tf.gather(increment, gather_indices), [(num_bonds[0], 0)])
+    pair_indices = pair_indices.merge_dims(outer_axis=0, inner_axis=1).to_tensor()
+    pair_indices = pair_indices + increment[:, tf.newaxis]
+    atom_features = atom_features.merge_dims(outer_axis=0, inner_axis=1).to_tensor()
+    bond_features = bond_features.merge_dims(outer_axis=0, inner_axis=1).to_tensor()
+
+    return (atom_features, bond_features, pair_indices, molecule_indicator), y_batch
+
+
+def MPNNDataset(X, y, batch_size=32, shuffle=False):
+    dataset = tf.data.Dataset.from_tensor_slices((X, (y)))
+    if shuffle:
+        dataset = dataset.shuffle(1024)
+    return dataset.batch(batch_size).map(prepare_batch, -1).prefetch(-1)
+
+class EdgeNetwork(layers.Layer):
+    def build(self, input_shape):
+        self.atom_dim = input_shape[0][-1]
+        self.bond_dim = input_shape[1][-1]
+        self.kernel = self.add_weight(
+            shape=(self.bond_dim, self.atom_dim * self.atom_dim),
+            initializer="glorot_uniform",
+            name="kernel",
+        )
+        self.bias = self.add_weight(
+            shape=(self.atom_dim * self.atom_dim), initializer="zeros", name="bias",
+        )
+        self.built = True
+
+    def call(self, inputs):
+        atom_features, bond_features, pair_indices = inputs
+
+        # Apply linear transformation to bond features
+        bond_features = tf.matmul(bond_features, self.kernel) + self.bias
+
+        # Reshape for neighborhood aggregation later
+        bond_features = tf.reshape(bond_features, (-1, self.atom_dim, self.atom_dim))
+
+        # Obtain atom features of neighbors
+        atom_features_neighbors = tf.gather(atom_features, pair_indices[:, 1])
+        atom_features_neighbors = tf.expand_dims(atom_features_neighbors, axis=-1)
+
+        # Apply neighborhood aggregation
+        transformed_features = tf.matmul(bond_features, atom_features_neighbors)
+        transformed_features = tf.squeeze(transformed_features, axis=-1)
+        aggregated_features = tf.math.unsorted_segment_sum(
+            transformed_features,
+            pair_indices[:, 0],
+            num_segments=tf.shape(atom_features)[0],
+        )
+        return aggregated_features
+
+
+class MessagePassing(layers.Layer):
+    def __init__(self, units, steps=4, **kwargs):
+        super().__init__(**kwargs)
+        self.units = units
+        self.steps = steps
+
+    def build(self, input_shape):
+        self.atom_dim = input_shape[0][-1]
+        self.message_step = EdgeNetwork()
+        self.pad_length = max(0, self.units - self.atom_dim)
+        self.update_step = layers.GRUCell(self.atom_dim + self.pad_length)
+        self.built = True
+
+    def call(self, inputs):
+        atom_features, bond_features, pair_indices = inputs
+
+        # Pad atom features if number of desired units exceeds atom_features dim.
+        # Alternatively, a dense layer could be used here.
+        atom_features_updated = tf.pad(atom_features, [(0, 0), (0, self.pad_length)])
+
+        # Perform a number of steps of message passing
+        for i in range(self.steps):
+            # Aggregate information from neighbors
+            atom_features_aggregated = self.message_step(
+                [atom_features_updated, bond_features, pair_indices]
+            )
+
+            # Update node state via a step of GRU
+            atom_features_updated, _ = self.update_step(
+                atom_features_aggregated, atom_features_updated
+            )
+        return atom_features_updated
+
+class PartitionPadding(layers.Layer):
+    def __init__(self, batch_size, **kwargs):
+        super().__init__(**kwargs)
+        self.batch_size = batch_size
+
+    def call(self, inputs):
+
+        atom_features, molecule_indicator = inputs
+
+        # Obtain subgraphs
+        atom_features_partitioned = tf.dynamic_partition(
+            atom_features, molecule_indicator, self.batch_size
+        )
+
+        # Pad and stack subgraphs
+        num_atoms = [tf.shape(f)[0] for f in atom_features_partitioned]
+        max_num_atoms = tf.reduce_max(num_atoms)
+        atom_features_stacked = tf.stack(
+            [
+                tf.pad(f, [(0, max_num_atoms - n), (0, 0)])
+                for f, n in zip(atom_features_partitioned, num_atoms)
+            ],
+            axis=0,
+        )
+
+        # Remove empty subgraphs (usually for last batch in dataset)
+        gather_indices = tf.where(tf.reduce_sum(atom_features_stacked, (1, 2)) != 0)
+        gather_indices = tf.squeeze(gather_indices, axis=-1)
+        return tf.gather(atom_features_stacked, gather_indices, axis=0)
+
+
+class TransformerEncoderReadout(layers.Layer):
+    def __init__(
+        self, num_heads=8, embed_dim=64, dense_dim=512, batch_size=32, **kwargs
+    ):
+        super().__init__(**kwargs)
+
+        self.partition_padding = PartitionPadding(batch_size)
+        self.attention = layers.MultiHeadAttention(num_heads, embed_dim)
+        self.dense_proj = keras.Sequential(
+            [layers.Dense(dense_dim, activation="relu"), layers.Dense(embed_dim),]
+        )
+        self.layernorm_1 = layers.LayerNormalization()
+        self.layernorm_2 = layers.LayerNormalization()
+        self.average_pooling = layers.GlobalAveragePooling1D()
+
+    def call(self, inputs):
+        x = self.partition_padding(inputs)
+        padding_mask = tf.reduce_any(tf.not_equal(x, 0.0), axis=-1)
+        padding_mask = padding_mask[:, tf.newaxis, tf.newaxis, :]
+        attention_output = self.attention(x, x, attention_mask=padding_mask)
+        proj_input = self.layernorm_1(x + attention_output)
+        proj_output = self.layernorm_2(proj_input + self.dense_proj(proj_input))
+        return self.average_pooling(proj_output)
+
+def MPNNModel(
+    atom_dim,
+    bond_dim,
+    batch_size=32,
+    message_units=64,
+    message_steps=4,
+    num_attention_heads=8,
+    dense_units=512,
+):
+
+    atom_features = layers.Input((atom_dim), dtype="float32", name="atom_features")
+    bond_features = layers.Input((bond_dim), dtype="float32", name="bond_features")
+    pair_indices = layers.Input((2), dtype="int32", name="pair_indices")
+    molecule_indicator = layers.Input((), dtype="int32", name="molecule_indicator")
+
+    x = MessagePassing(message_units, message_steps)(
+        [atom_features, bond_features, pair_indices]
+    )
+
+    x = TransformerEncoderReadout(
+        num_attention_heads, message_units, dense_units, batch_size
+    )([x, molecule_indicator])
+
+    x = layers.Dense(dense_units, activation="relu")(x)
+    x = layers.Dense(1, activation="sigmoid")(x)
+
+    model = keras.Model(
+        inputs=[atom_features, bond_features, pair_indices, molecule_indicator],
+        outputs=[x],
+    )
+    return model
+
+
+mpnn = MPNNModel(
+    atom_dim=x_train[0][0][0].shape[0], bond_dim=x_train[1][0][0].shape[0],
+)
+
+mpnn.compile(
+    loss=keras.losses.BinaryCrossentropy(),
+    optimizer=keras.optimizers.Adam(learning_rate=5e-4),
+    metrics=[keras.metrics.AUC(name="AUC")],
+)
+
+keras.utils.plot_model(mpnn, show_dtype=True, show_shapes=True)
+
+train_dataset = MPNNDataset(x_train, y_train)
+valid_dataset = MPNNDataset(x_valid, y_valid)
+test_dataset = MPNNDataset(x_test, y_test)
+# zwc add
+# class PrintLearningRate(tf.keras.callbacks.Callback):
+#     def on_epoch_end(self, epoch, logs=None):
+#         lr = self.model.optimizer.lr
+#         if hasattr(self.model.optimizer, '_decayed_lr'):
+#             lr = self.model.optimizer._decayed_lr(tf.float32).numpy()
+#         print(f'Learning rate for epoch {epoch + 1} is {lr}')
+lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(
+    monitor='val_loss', factor=0.1, patience=20
+)
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss', patience=20, restore_best_weights=True
+)
+# print_lr = PrintLearningRate() 
+# zwc add
+history = mpnn.fit(
+    train_dataset,
+    validation_data=valid_dataset,
+    epochs=80,
+    verbose=2,
+    class_weight={0: 2.0, 1: 0.5},
+    callbacks=[lr_scheduler, early_stopping],#,print_lr],# zwc add
+)
+
+plt.figure(figsize=(10, 6))
+plt.plot(history.history["AUC"], label="train AUC")
+plt.plot(history.history["val_AUC"], label="valid AUC")
+plt.xlabel("Epochs", fontsize=16)
+plt.ylabel("AUC", fontsize=16)
+plt.legend(fontsize=16)
+plt.savefig('auc_plot.png')  # 您可以指定其他文件路径和文件名
+plt.close()
+
+molecules = [molecule_from_smiles(df.smiles.values[index]) for index in test_index]
+y_true = [df.p_np.values[index] for index in test_index]
+y_pred = tf.squeeze(mpnn.predict(test_dataset), axis=1)
+
+# legends = [f"y_true/y_pred = {y_true[i]}/{y_pred[i]:.2f}" for i in range(len(y_true))]
+# print(MolsToGridImage(molecules, molsPerRow=4, legends=legends))
+
+
+
+# Assuming new test data is in 'new_test_data.csv'
+# new_test_csv_path = "./mini_train_bbbp.csv"
+new_test_csv_path = "./QSAR_test_bbbp.csv"
+
+new_test_df = pd.read_csv(new_test_csv_path)
+
+x_new_test = graphs_from_smiles(new_test_df.smiles)
+y_new_test = new_test_df.p_np
+new_test_dataset = MPNNDataset(x_new_test, y_new_test)
+
+# Preprocess the new test data
+
+# Predict using the trained model
+y_new_pred = tf.squeeze(mpnn.predict(new_test_dataset), axis=1)
+
+# Combine names and predictions into a DataFrame
+predictions_df = pd.DataFrame({
+    'name': new_test_df.name,
+    'predict_score': y_new_pred.numpy()
+})
+
+# Save the predictions to a CSV file
+predictions_csv_path = './predicted_scores.csv'
+predictions_df.to_csv(predictions_csv_path, index=False)
