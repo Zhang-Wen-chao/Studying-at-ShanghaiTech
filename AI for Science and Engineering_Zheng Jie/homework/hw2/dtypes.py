4c25a126b1cb333b76183c4b581195d4dc912eec
Zhang-Wen-chao <zhangwch2022@shanghaitech.edu.cn>
2024-05-25 04:48:11 +0000

    update

--- AI for Science and Engineering_Zheng Jie/homework/hw2/dtypes.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# 读取CSV文件
-df = pd.read_csv('predictions.csv')
-
-# 打印出每列的数据类型
-print(df.dtypes)
--- AI for Science and Engineering_Zheng Jie/homework/hw2/mpnn_predict.py
+++
@@ -1,7 +0,0 @@
-# 创建模型
-mpnn = MPNNModel(
-    atom_dim=x_train[0][0][0].shape[0], bond_dim=x_train[1][0][0].shape[0],
-)
-
-# 加载保存的权重
-mpnn.load_weights('path_to_save_model_weights.h5')
